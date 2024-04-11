80d19eb6d203f1fd0f15cfc34a305114972a7aff
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 16:17:44 +0000

    rock paper scissors paths

--- app.rb
+++ app.rb
@@ -4,3 +4,24 @@ require "sinatra/reloader"
 get("/") do
  erb(:home)
 end
+
+get("/rock") do
+  choices = ["rock", "paper", "scissors"]
+  index = rand(0..2)
+  @their_move = choices[index] 
+  erb(:rock)
+end
+
+get ("/paper") do
+  choices = ["rock", "paper", "scissors"]
+  index = rand(0..2)
+  @their_move = choices[index] 
+  erb(:paper)
+end
+
+get ("/scissors") do
+  choices = ["rock", "paper", "scissors"]
+  index = rand(0..2)
+  @their_move = choices[index] 
+  erb(:scissors)
+end
